449446436ac875d3ca2fc8e33416e43017e931cb
Zach Keskinen <zachkeskinen@gmail.com>
2022-07-19 12:02:53 -0600

    moving files from old repo over

---
+++ scripts/dir_space_strip.py
@@ -0,0 +1,19 @@
+import os
+parent_dir = '../test/'
+
+def replace(parent, replace = '_'):
+    print(f'Warning! About to replace all whitespaces with "{replace}"s in {os.path.abspath(parent)}')
+    print('Press y to continue...')
+    ans = input()
+    if ans.lower() == 'y':
+        for path, folders, files in os.walk(parent):
+            for f in files:
+                os.rename(os.path.join(path, f), os.path.join(path, f.replace(' ', replace)))
+            for i in range(len(folders)):
+                new_name = folders[i].replace(' ', replace)
+                os.rename(os.path.join(path, folders[i]), os.path.join(path, new_name))
+                folders[i] = new_name
+    else:
+        print(f'Passing...')
+
+replace(parent_dir)

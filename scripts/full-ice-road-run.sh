9887130e9c03501af6f23be52d6a2ec0daa264aa
ZachKeskinen <zachkeskinen@gmail.com>
2022-09-15 13:57:51 -0600

    shell work

--- scripts/full-ice-road-run.sh
+++ scripts/full-ice-road-run.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 ASP_DIR='/SNOWDATA/IDALS/ASP/bin/'
-MCS_SHP='/SNOWDATA/IDALS/ice-road-copters/transform_area/hwy_21/hwy_21_utm_edit_v2.shp'
-DC_SHP='/SNOWDATA/IDALS/ice-road-copters/transform_area/BogusBasin/Roads_DryCreek.shp'
+MCS_SHP='/home/zacharykeskinen/ice-road-copters/transform_area/hwy_21/hwy_21_utm_edit_v2.shp'
+DC_SHP='/home/zacharykeskinen/ice-road-copters/transform_area/BogusBasin/Roads_DryCreek.shp'
 MCS_DEM='/SNOWDATA/IDALS/REF_DEM/MCS_REFDEM_WGS84.tif'
 DC_DEM='/SNOWDATA/IDALS/REF_DEM/DC_REFDEM_WGS84.tif'
 
-ICEROAD='/SNOWDATA/IDALS/ice-road-copters/scripts/ice-road-pipeline.py'
+ICEROAD=/home/zacharykeskinen/ice-road-copters/scripts/ice-road-pipeline.py'
 
 D2021='/SNOWDATA/IDALS/2021'
 D2022='/SNOWDATA/IDALS/2022'
-
-# Change to your scratch...
 SCRATCH='/home/zacharykeskinen/scratch'
-
 echo "Starting 2021 Flights..."
 
 for FP in $(ls $D2021)
 do
-    $(rm -r $D2021/$FP/ice-road)
     $(cp -r $D2021/$FP $SCRATCH)
+    $(rm -r $SCRATCH/$FP/ice-road)
     if [[ $FP == *"MCS"* ]]; then
         MCS_CMD="python $ICEROAD $SCRATCH/$FP -e $MCS_DEM -s $MCS_SHP -a $ASP_DIR"
         echo $MCS_CMD
@@ -36,12 +33,12 @@ do
     $(rm -r $SCRATCH/$FP)
 done
 
-echo $'\nStarting 2022 Flights...'
+echo "Starting 2022 Flights..."
 
 for FP in $(ls $D2022)
 do
-    $(rm -r $D2022/$FP/ice-road)
     $(cp -r $D2022/$FP $SCRATCH)
+    $(rm -r $SCRATCH/$FP/ice-road)
     if [[ $FP == *"MCS"* ]]; then
         MCS_CMD="python $ICEROAD $SCRATCH/$FP -e $MCS_DEM -s $MCS_SHP -a $ASP_DIR"
         echo $MCS_CMD

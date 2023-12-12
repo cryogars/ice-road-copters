c1152564b5d8b6c8080b08d81e95c7db6783e062
brentwilder <brentonwilder1995@gmail.com>
2023-12-12 15:13:27 -0800

    typo

--- scripts/las_ssa_prep.R
+++ scripts/las_ssa_prep.R
@@ -84,7 +84,7 @@ df$Z_h <- df$Z_h - d_shift # ASP shift applied here (D)
 # COMPUTE RFL
 df <- df %>%
     mutate(
-      cosi = ((X_h-X)*n_i + (Y_h-Y)*n_j + (Z_h-Z)*n_k) / (sqrt( (X_h-X)^2 + (Y_h-Y)^2 + (Z_h-Z)^2) * sqrt(n_i^2 + n_j^2 +n_k^2))
+      cosi = ((X_h-X)*n_i + (Y_h-Y)*n_j + (Z_h-Z)*n_k) / (sqrt( (X_h-X)^2 + (Y_h-Y)^2 + (Z_h-Z)^2) * sqrt(n_i^2 + n_j^2 +n_k^2)),
       rfl = (10^(Reflectance/10) * road_cal_factor) / cosi
         )
 

4d0bcb3328cac479549efc0d75a9883454b2ac35
Naheem Adebisi <adebisi.naheem@gmail.com>
2024-04-11 12:56:23 -0600

    modify snow_pc script

--- contrib/naheem/snow_pc.py
+++ contrib/naheem/snow_pc.py
@@ -35,7 +35,7 @@ def pc2uncorrectedDEM(in_dir, outlas = '', outtif = '', user_dem = '', dem_low =
     return dtm_laz, dtm_tif, dsm_laz, dsm_tif
 
 
-def pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = ''):
+def pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = '', dem_low = 20, dem_high = 50, mean_k = 20, multiplier = 3, lidar_pc = 'yes'):
     """Converts laz files to corrected DEM.
 
     Args:
@@ -50,7 +50,7 @@ def pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = ''):
 
 
     # create uncorrected DEM
-    dtm_laz, dtm_tif, dsm_laz, dsm_tif = pc2uncorrectedDEM(in_dir, user_dem = user_dem)
+    dtm_laz, dtm_tif, dsm_laz, dsm_tif = pc2uncorrectedDEM(in_dir, user_dem = user_dem, dem_low = 20, dem_high = 50, mean_k = 20, multiplier = 3, lidar_pc = 'yes')
 
 
     # align the point cloud
@@ -59,7 +59,7 @@ def pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = ''):
 
     return dtm_align_tif, dsm_align_tif
 
-def pc2snow(in_dir, align_file, asp_dir, user_dem = ''):
+def pc2snow(in_dir, align_file, asp_dir, user_dem = '', dem_low = 20, dem_high = 50, mean_k = 20, multiplier = 3, lidar_pc = 'yes'):
     """Converts laz files to snow depth and canopy height.
 
     Args:
@@ -73,7 +73,7 @@ def pc2snow(in_dir, align_file, asp_dir, user_dem = ''):
     """
 
     # create corrected DEM
-    dtm_align_tif, dsm_align_tif = pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = user_dem)
+    dtm_align_tif, dsm_align_tif = pc2correctedDEM(in_dir, align_file, asp_dir, user_dem = user_dem, dem_low = 20, dem_high = 50, mean_k = 20, multiplier = 3, lidar_pc = 'yes')
     
     #set dem_fp
     in_dir = os.path.dirname(dtm_align_tif)
@@ -94,145 +94,3 @@ def pc2snow(in_dir, align_file, asp_dir, user_dem = ''):
 
     return snow_depth_path, canopy_height_path
 
-
-# class Map(ipyleaflet.Map):
-#     """Custom map class that inherits from ipyleaflet.Map.
-#     """
-#     def __init__(self, *args, **kwargs):
-
-#         if "scroll_wheel_zoom" not in kwargs:
-#             kwargs["scroll_wheel_zoom"] = True
-#         super().__init__(*args, **kwargs)
-
-#         if "layers_control" not in kwargs:
-#             kwargs["layers_control"] = True
-
-#         if kwargs["layers_control"]:
-#             self.add_LayerControl()
-
-#         if "fullscreen_control" not in kwargs:
-#             kwargs["fullscreen_control"] = True
-
-#         if kwargs["fullscreen_control"]:
-#             self.add_fullscreen_control()            
-
-#     def add_search_control(self, position = "topleft", **kwargs):
-#         """Add a search control to the map.
-
-#         Args:
-#             position (str, optional): Position of the search control. Defaults to "topleft".
-
-#         Returns:
-#             _type_: SearchControl object.
-#         """
-#         if "url" not in kwargs:
-#             kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"
-#         search = ipyleaflet.SearchControl(position = position, **kwargs)
-#         self.add_control(search)
-#         return search
-
-#     def add_LayerControl(self, position = "topright"):
-#         """Add a layer control to the map.
-
-#         Args:
-#             position (str, optional): Position of the layer control. Defaults to "topright".
-
-#         Returns:
-#             _type_: LayerControl object.
-#         """
-#         layer_control = ipyleaflet.LayersControl(position = position)
-#         self.add_control(layer_control)
-#         return layer_control
-    
-#     def add_fullscreen_control(self, position = "topright"):
-#         """Add a fullscreen control to the map.
-
-#         Args:
-#             position (str, optional): Position of the fullscreen control. Defaults to "topright".
-
-#         Returns:
-#             _type_: FullscreenControl object.
-#         """
-#         fullscreen = ipyleaflet.FullScreenControl(position = position)
-#         self.add_control(fullscreen)
-#         return fullscreen
-    
-#     def add_tile_layer(self, url, name, **kwargs):
-#         """Add a tile layer to the map.
-
-#         Args:
-#             url (str): URL of the tile layer.
-
-#         Returns:
-#             _type_: TileLayer object.
-#         """
-#         tile_layer = ipyleaflet.TileLayer(url = url, name=name, **kwargs)
-#         self.add_layer(tile_layer)
-#         return tile_layer
-    
-#     def add_basemap(self, basemap, **kwargs):
-#         """Add a basemap to the map.
-
-#         Args:
-#             basemap (_type_): A string representing the basemap to add.
-
-#         Raises:
-#             ValueError: If the basemap is not recognized.
-#         """
-#         import xyzservices.providers as xyz
-#         if basemap.lower() == "openstreetmap":
-#             url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-#             self.add_tile_layer(url, name = basemap,**kwargs)
-#         elif basemap.lower() == "stamen terrain":
-#             url = "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png"
-#             self.add_tile_layer(url, name = basemap,**kwargs)
-#         elif basemap.lower() == "opentopomap":
-#             url = "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-#             self.add_tile_layer(url, name = basemap,**kwargs)
-#         elif basemap.lower() == "satellite":
-#             url = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-#             self.add_tile_layer(url, name = basemap,**kwargs)
-
-#         else:
-#             try:
-#                 basemap = eval(f"xyz.{basemap}")
-#                 url = basemap.build_url()
-#                 name = basemap["name"]
-#                 attribute = basemap["attribution"]
-#                 print(url, name)
-#                 self.add_tile_layer(url, name, attribution = attribute, **kwargs)
-#             except:
-#                 raise ValueError(f"Basemap {basemap} not recognized.")
-
-#     def add_geojson(self, data, name = "geojson", **kwargs):
-#         """Add a GeoJSON layer to the map.
-
-#         Args:
-#             data (_type_): A GeoJSON object.
-
-#         Returns:
-#             _type_: GeoJSON object.
-#         """
-
-#         if isinstance(data, str):
-#             import json
-#             with open(data, 'r') as f:
-#                 data = json.load(f)
-#         geojson = ipyleaflet.GeoJSON(data = data, name = name, **kwargs)
-#         self.add_layer(geojson)
-#         return geojson
-    
-#     def add_shp(self, data, name = "shapefile", **kwargs):
-        # """Add a shapefile to the map.
-
-        # Args:
-        #     data (_type_): A shapefile object.
-
-        # Returns:
-        #     _type_: GeoData object.
-        # """
-        # gdf = gpd.read_file(data)
-        # geojson = gdf.__geo_interface__
-        # self.add_geojson(geojson, name = name, **kwargs)
-        
-        

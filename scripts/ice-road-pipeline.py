a0db502529a44eab7c1c41696a6babbccb76e04c
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2025-11-13 15:00:20 -0700

    added smrf filter parameters

--- scripts/ice-road-pipeline.py
+++ scripts/ice-road-pipeline.py
@@ -2,7 +2,9 @@
 Takes input directory full of .laz (or.las) files and filters+classifies them to DTM laz and DTM tif.
 
 Usage:
-    ice-road-pipeline.py <in_dir> [-e user_dem] [-d debug] [-a asp_dir] [-s shp_fp] [-b buffer_meters] [-r shp_fp_rfl] [-i imu_data] [-c cal_las] [-k known_rfl] [-h h2o] [-o aod]  
+    ice-road-pipeline.py <in_dir> -s shp_fp [-e user_dem] [-d debug] [-a asp_dir] [-b buffer_meters] [-g geoid]
+                               [-r shp_fp_rfl] [-i imu_data] [-c cal_las] [-k known_rfl] [-h h2o] [-o aod]
+                               [-S smrf_scalar] [-L smrf_slope] [-T smrf_threshold] [-W smrf_window]  
 
 Options:
     -e user_dem      Path to user specifed DEM
@@ -21,10 +23,10 @@ Options:
     -k known_rfl     (Optional) Known intrinsic reflectance at 1064nm (float/real) for target identified in shp_fp_rfl.
     -h h2o           (Optional) Water Column Vapor in atmosphere in mm (float) 
     -o aod           (Optional) Aerosol optical depth at 550 nm (float)
-    -scalar          (Optional) Scalar parameter for SMRF
-    -slope           (Optional) slope parameter for SMRF
-    -threshold       (Optional) threshold parameter for SMRF
-    -window          (Optional) window parameter for SMRF
+    -S smrf_scalar          (Optional) Scalar parameter for SMRF
+    -L smrf_slope           (Optional) slope parameter for SMRF
+    -T smrf_threshold       (Optional) threshold parameter for SMRF
+    -W smrf_window          (Optional) window parameter for SMRF
 
 """
 
@@ -53,7 +55,6 @@ if __name__ == '__main__':
     # get command line args
     args = docopt(__doc__)
     user_dem = args.get('-e')
-    smrf_classifier_args={}
     if user_dem:
         user_dem = abspath(user_dem)
     geoid = args.get('-g')
@@ -110,26 +111,11 @@ if __name__ == '__main__':
     if aod:
         aod = float(aod)
 
-    scalar = args.get('-scalar')
-    if scalar:
-        scalar = float(scalar)
-        smrf_classifier_args['scalar'] = scalar
-
-    slope = args.get('-slope')
-    if slope:
-        slope = float(slope)
-        smrf_classifier_args['slope'] = slope
-
-    threshold = args.get('-threshold')
-    if threshold:
-        threshold = float(threshold)
-        smrf_classifier_args['threshold'] = threshold
-
-    window = args.get('-window')
-    if window:
-        window = float(window)
-        smrf_classifier_args['window'] = window
-
+    smrf_overrides = {}
+    for flag, key in [('-S', 'scalar'), ('-L', 'slope'), ('-T', 'threshold'), ('-W', 'window')]:
+        value = args.get(flag)
+        if value is not None:
+            smrf_overrides[key] = float(value)
 
     in_dir = args.get('<in_dir>')
     # convert to abspath
@@ -177,9 +163,14 @@ if __name__ == '__main__':
     log.info('Starting laz2uncorrectedDEM')
     log.info(f'Using in_dir: {in_dir}, user_dem: {user_dem}')
 
-    outtif, outlas, canopy_laz = las2uncorrectedDEM(in_dir, debug, log, smrf_classifier_args,
-                                                    user_dem = user_dem, 
-                                                    las_extra_byte_format = las_extra_byte_format)
+    outtif, outlas, canopy_laz = las2uncorrectedDEM(
+        in_dir,
+        debug,
+        log,
+        user_dem=user_dem,
+        las_extra_byte_format=las_extra_byte_format,
+        smrf_overrides=smrf_overrides
+    )
 
     log.info('Starting ASP laz align')
     log.info(f'Using in_dir: {in_dir}, shapefile: {shp_fp}, ASP dir: {asp_dir}')
